29a17a531578d2178e298fae5c7eebe49b941c7e
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-05-15 15:03:56 +0530

    k

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-//const cors = require('cors');
+const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
